b0dbb450bbedab6e3853a679f5521a1ebdfa32d8
Michal Czyzewski <czy.mich@gmail.com>
2019-04-27 12:02:16 +0200

    add +=1 to two data types

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -9,6 +9,11 @@ public class Main {
         System.out.println("Numbber as String : " + number);
         int intNumber = Integer.parseInt(number);
         System.out.println("Number as Integer  : " + intNumber);
+        number += 1;
+        intNumber +=1;
+        System.out.println("============================");
+        System.out.println("Number as String " + number);
+        System.out.println("Number as Integer " + intNumber);
 
     }
 }
