e4631ebc45a76794ececcdd65addd328c284410c
Michal Czyzewski <czy.mich@gmail.com>
2019-04-27 12:00:13 +0200

    add Integer.parseInt

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -3,6 +3,12 @@ package pl.mczyzewski;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        String number = "2018";
+        System.out.println("Numbber as String : " + number);
+        int intNumber = Integer.parseInt(number);
+        System.out.println("Number as Integer  : " + intNumber);
+
     }
 }
